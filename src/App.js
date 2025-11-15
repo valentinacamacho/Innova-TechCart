f692c5c67827e5470df3ad929d38da33438ae807
valentinacamacho <valentinacamacho0206@gmail.com>
2025-11-15 06:37:38 -0500

    feat: agregar funcionalidad de agregar productos al carrito en app.js donde revisa si el producto aumenta la cantidad sin aumentar el stock y si no lo agrega a cantidad 1.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Layout/Header";
 import ListProducts from "./components/ListProducts";
 
 function App() {
+  const [card, setCart] = useState([]);
+  useEffect(() => {
+    const storedCard = localStorage.getItem("card");
+    if (storedCard) setCart(JSON.parse(storedCard));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("card", JSON.stringify(card));
+  }, [card]);
+
+  const onAddProduct = (product) => {
+    setCart((prevCart) => {
+      const productExist = prevCart.find((p) => p.id === product.id);
+      if (productExist) {
+        return prevCart.map((p) =>
+          p.id === product.id
+            ? { ...p, quantity: Math.min(p.quantity + 1, product.stock) }
+            : p
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
   return (
     <div>
-      <Header />
-      <ListProducts/>
+      <Header cardCount={card.reduce((sum, p) => sum + p.quantity, 0)} />
+      <ListProducts onAddProduct={onAddProduct} />
     </div>
   );
 }
