32ce075b3efd99c78fdf16aec9dd60bf16837cca
valentinacamacho <valentinacamacho0206@gmail.com>
2025-11-14 23:55:13 -0500

    feat:The ListPro.css file was created to add responsive styles to the Listcard columns and rows; responsive desktop, mobile, and tablet versions were also added.

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import { products } from "../data/products";
+import "../styles/ListPro.css";
 
 function ListProducts() {
   return (
-    <div>
+    <div className="products-list">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
