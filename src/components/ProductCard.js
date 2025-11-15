44725c1dca2fa13704af0020a8f7c3ba76f10347
valentinacamacho <valentinacamacho0206@gmail.com>
2025-11-14 23:19:14 -0500

    feat: The ProCard.css file was created with classes for custom design, styles were applied to each container, hover animations were added, and conditional colors were applied according to stock.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,34 @@
 import React from "react";
+import ProCard from "../styles/ProCard.css";
 
 function ProductCard({ product }) {
   return (
-    <div>
-      {product.urlImage ? (
-        <img src={product.urlImage} alt={product.name} />
-      ) : (
-        <span>Sin img</span>
-      )}
+    <div className="product-card">
+      <div className="product-img">
+        {product.urlImage ? (
+          <img src={product.urlImage} alt={product.name} />
+        ) : (
+          <span>Sin img</span>
+        )}
+      </div>
 
-      <h3>{product.marca}</h3>
-      <p>{product.name}</p>
-      <p>{product.description}</p>
+      <div className="product-info">
+        <h3 className="product-brand">{product.marca}</h3>
+        <p className="product-name">{product.name}</p>
+        {product.description && (
+          <p className="product-description">{product.description}</p>
+        )}
+        <p className="product-price">${product.price}</p>
 
-      <p>{product.stock > 0 ? `Stock: ${product.stock}` : "sin stock}"}</p>
-      <button disabled={product.stock === 0}>Añadir al carrito</button>
+        <p
+          className={`product-stock ${
+            product.stock > 0 ? "Disponible" : "Agotado"
+          }`}
+        >
+          {product.stock > 0 ? `Stock: ${product.stock}` : "Sin stock"}
+        </p>
+        <button disabled={product.stock === 0}>Añadir al carrito</button>
+      </div>
     </div>
   );
 }
